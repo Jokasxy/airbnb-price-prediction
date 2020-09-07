f825ca76f88df27fccf0a726a5be41c098bc9aca
Jokasxy <josip_1997@live.com>
2020-09-07 21:43:07 +0200

    Express removed

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,6 @@ import { Controller, useForm } from 'react-hook-form';
 import './App.css';
 import axios from 'axios';
 
-const path = require('path');
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
-app.get('*', (req: any, res: any) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
-app.listen(port, () => {
-   console.log(`Server is up on port ${port}!`);
-});
-
 enum FormData {
     id = 'id',
     name = 'name',
