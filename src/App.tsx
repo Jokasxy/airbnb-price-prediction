1c15142233d0e12cefb5655b2d9748160efc2ceb
Jokasxy <josip_1997@live.com>
2020-09-07 21:26:36 +0200

    Express configuration

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,19 @@ import { Controller, useForm } from 'react-hook-form';
 import './App.css';
 import axios from 'axios';
 
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
+app.listen(port, () => {
+   console.log(`Server is up on port ${port}!`);
+});
+
 enum FormData {
     id = 'id',
     name = 'name',
