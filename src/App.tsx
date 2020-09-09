4ba5de47d99d9f910b6ad8834a2a3934d0c4b4e7
Josip Blažević <jblazevic@binaryx.hr>
2020-09-09 10:03:23 +0200

    Loading spinner added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import { makeStyles, FormControl, Input, InputLabel, Button, Select, MenuItem } from '@material-ui/core';
+import {
+	makeStyles,
+	FormControl,
+	Input,
+	InputLabel,
+	Button,
+	Select,
+	MenuItem,
+	CircularProgress,
+} from '@material-ui/core';
 import RefreshIcon from '@material-ui/icons/Refresh';
 import { Controller, useForm } from 'react-hook-form';
 import './App.css';
@@ -39,6 +48,7 @@ interface Result {
 const App = () => {
 	const classes = useClasses();
 	const [result, setResult] = React.useState<Result | undefined>();
+	const [loading, setLoading] = React.useState(false);
 	const { handleSubmit, control } = useForm<Record<FormData, any>>();
 
 	const upperBound = React.useMemo(() => {
@@ -57,6 +67,7 @@ const App = () => {
 
 	const onSubmit = React.useCallback((values: Record<FormData, any>) => {
 		const data: any = requestData(values);
+		setLoading(true);
 		axios
 			.post('https://cors-anywhere.herokuapp.com/' + process.env.REACT_APP_API_URL!, data, {
 				headers: {
@@ -66,6 +77,7 @@ const App = () => {
 				},
 			})
 			.then((response) => {
+				setLoading(false);
 				setResult({
 					price: response.data.Results.output1.value.Values[0][5],
 					scoredLabelMean: response.data.Results.output1.value.Values[0][11],
@@ -82,196 +94,231 @@ const App = () => {
 			<header className={classes.header}>
 				<h1>Airbnb Price Prediction</h1>
 			</header>
-			<main className={classes.main}>
-			{ result ? (
-				<section className={classes.section}>
-					<p>Accomodation price is: {Number(result.price).toFixed(2)}</p>
-					<p>Predicted price is: {Number(result.scoredLabelMean).toFixed(2)}</p>
-					<p>Predicted price deviation is: {Number(result.scoredLabelStandardDeviation).toFixed(2)}</p>
-					{Number(result.price) > upperBound && <p className={classes.expensive}>Too expensive!</p>}
-					{Number(result.price) < lowerBound && <p className={classes.cheap}>Cheap accomodation!</p>}
-					{Number(result.price) > lowerBound && result.price < upperBound && <p>Reasonable price!</p>}
-					<Button
-						variant="contained"
-						color="primary"
-						className={classes.button}
-						startIcon={<RefreshIcon />}
-						onClick={() => setResult(undefined)}
-					>
-						AGAIN
-					</Button>
-				</section>
+			{loading ? (
+				<CircularProgress color="inherit" className={classes.spinner} />
 			) : (
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.id} className={classes.inputLabel}>
-							{FormData.id.toUpperCase()}
-						</InputLabel>
-						<Controller as={<Input />} control={control} defaultValue="" name={FormData.id} />
-					</FormControl>
+				<main className={classes.main}>
+					{result ? (
+						<section className={classes.section}>
+							<p>Accomodation price is: {Number(result.price).toFixed(2)}</p>
+							<p>Predicted price is: {Number(result.scoredLabelMean).toFixed(2)}</p>
+							<p>
+								Predicted price deviation is: {Number(result.scoredLabelStandardDeviation).toFixed(2)}
+							</p>
+							{Number(result.price) > upperBound && <p className={classes.expensive}>Too expensive!</p>}
+							{Number(result.price) < lowerBound && <p className={classes.cheap}>Cheap accomodation!</p>}
+							{Number(result.price) > lowerBound && result.price < upperBound && <p>Reasonable price!</p>}
+							<Button
+								variant="contained"
+								color="primary"
+								className={classes.button}
+								startIcon={<RefreshIcon />}
+								onClick={() => setResult(undefined)}>
+								AGAIN
+							</Button>
+						</section>
+					) : (
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.id} className={classes.inputLabel}>
+									{FormData.id.toUpperCase()}
+								</InputLabel>
+								<Controller as={<Input />} control={control} defaultValue="" name={FormData.id} />
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.name} className={classes.inputLabel}>
-							{FormData.name.toUpperCase()}
-						</InputLabel>
-						<Controller as={<Input />} control={control} defaultValue="" name={FormData.name} />
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.name} className={classes.inputLabel}>
+									{FormData.name.toUpperCase()}
+								</InputLabel>
+								<Controller as={<Input />} control={control} defaultValue="" name={FormData.name} />
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.host_id} className={classes.inputLabel}>
-							{FormData.host_id.toUpperCase()}
-						</InputLabel>
-						<Controller as={<Input />} control={control} defaultValue="" name={FormData.host_id} />
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.host_id} className={classes.inputLabel}>
+									{FormData.host_id.toUpperCase()}
+								</InputLabel>
+								<Controller as={<Input />} control={control} defaultValue="" name={FormData.host_id} />
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.host_name} className={classes.inputLabel}>
-							{FormData.host_name.toUpperCase()}
-						</InputLabel>
-						<Controller as={<Input />} control={control} defaultValue="" name={FormData.host_name} />
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.host_name} className={classes.inputLabel}>
+									{FormData.host_name.toUpperCase()}
+								</InputLabel>
+								<Controller
+									as={<Input />}
+									control={control}
+									defaultValue=""
+									name={FormData.host_name}
+								/>
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.neighbourhood_group} className={classes.inputLabel}>
-							{FormData.neighbourhood_group.toUpperCase()}
-						</InputLabel>
-						<Controller
-							as={
-								<Select className={classes.select}>
-									{neighbourhoodGroups.map((item, key) =>
-										<MenuItem key={key} value={item}>
-											{item}
-										</MenuItem>
-									)}
-								</Select>
-							}
-							control={control}
-							defaultValue=""
-							name={FormData.neighbourhood_group}
-						/>
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.neighbourhood_group} className={classes.inputLabel}>
+									{FormData.neighbourhood_group.toUpperCase()}
+								</InputLabel>
+								<Controller
+									as={
+										<Select className={classes.select}>
+											{neighbourhoodGroups.map((item, key) => (
+												<MenuItem key={key} value={item}>
+													{item}
+												</MenuItem>
+											))}
+										</Select>
+									}
+									control={control}
+									defaultValue=""
+									name={FormData.neighbourhood_group}
+								/>
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.neighbourhood} className={classes.inputLabel}>
-							{FormData.neighbourhood.toUpperCase()}
-						</InputLabel>
-						<Controller as={
-								<Select className={classes.select}>
-									{neighbourhoods.map((item, key) =>
-										<MenuItem key={key} value={item}>
-											{item}
-										</MenuItem>
-									)}
-								</Select>
-							}
-							control={control}
-							defaultValue=""
-							name={FormData.neighbourhood} />
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.neighbourhood} className={classes.inputLabel}>
+									{FormData.neighbourhood.toUpperCase()}
+								</InputLabel>
+								<Controller
+									as={
+										<Select className={classes.select}>
+											{neighbourhoods.map((item, key) => (
+												<MenuItem key={key} value={item}>
+													{item}
+												</MenuItem>
+											))}
+										</Select>
+									}
+									control={control}
+									defaultValue=""
+									name={FormData.neighbourhood}
+								/>
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.latitude} className={classes.inputLabel}>
-							{FormData.latitude.toUpperCase()}
-						</InputLabel>
-						<Controller as={<Input />} control={control} defaultValue="" name={FormData.latitude} />
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.latitude} className={classes.inputLabel}>
+									{FormData.latitude.toUpperCase()}
+								</InputLabel>
+								<Controller as={<Input />} control={control} defaultValue="" name={FormData.latitude} />
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.longitude} className={classes.inputLabel}>
-							{FormData.longitude.toUpperCase()}
-						</InputLabel>
-						<Controller as={<Input />} control={control} defaultValue="" name={FormData.longitude} />
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.longitude} className={classes.inputLabel}>
+									{FormData.longitude.toUpperCase()}
+								</InputLabel>
+								<Controller
+									as={<Input />}
+									control={control}
+									defaultValue=""
+									name={FormData.longitude}
+								/>
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.room_type} className={classes.inputLabel}>
-							{FormData.room_type.toUpperCase()}
-						</InputLabel>
-						<Controller as={
-							<Select className={classes.select}>
-								{roomTypes.map((item, key) =>
-									<MenuItem key={key} value={item}>
-										{item}
-									</MenuItem>
-								)}
-							</Select>
-							}
-							control={control}
-							defaultValue=""
-							name={FormData.room_type}
-						/>
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.room_type} className={classes.inputLabel}>
+									{FormData.room_type.toUpperCase()}
+								</InputLabel>
+								<Controller
+									as={
+										<Select className={classes.select}>
+											{roomTypes.map((item, key) => (
+												<MenuItem key={key} value={item}>
+													{item}
+												</MenuItem>
+											))}
+										</Select>
+									}
+									control={control}
+									defaultValue=""
+									name={FormData.room_type}
+								/>
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.price} className={classes.inputLabel}>
-							{FormData.price.toUpperCase()}
-						</InputLabel>
-						<Controller as={<Input />} control={control} defaultValue="" name={FormData.price} />
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.price} className={classes.inputLabel}>
+									{FormData.price.toUpperCase()}
+								</InputLabel>
+								<Controller as={<Input />} control={control} defaultValue="" name={FormData.price} />
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.minimum_nights} className={classes.inputLabel}>
-							{FormData.minimum_nights.toUpperCase()}
-						</InputLabel>
-						<Controller as={<Input />} control={control} defaultValue="" name={FormData.minimum_nights} />
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.minimum_nights} className={classes.inputLabel}>
+									{FormData.minimum_nights.toUpperCase()}
+								</InputLabel>
+								<Controller
+									as={<Input />}
+									control={control}
+									defaultValue=""
+									name={FormData.minimum_nights}
+								/>
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.number_of_reviews} className={classes.inputLabel}>
-							{FormData.number_of_reviews.toUpperCase()}
-						</InputLabel>
-						<Controller
-							as={<Input />}
-							control={control}
-							defaultValue=""
-							name={FormData.number_of_reviews}
-						/>
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.number_of_reviews} className={classes.inputLabel}>
+									{FormData.number_of_reviews.toUpperCase()}
+								</InputLabel>
+								<Controller
+									as={<Input />}
+									control={control}
+									defaultValue=""
+									name={FormData.number_of_reviews}
+								/>
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.last_review} className={classes.inputLabel}>
-							{FormData.last_review.toUpperCase()}
-						</InputLabel>
-						<Controller as={<Input />} control={control} defaultValue="" name={FormData.last_review} />
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.last_review} className={classes.inputLabel}>
+									{FormData.last_review.toUpperCase()}
+								</InputLabel>
+								<Controller
+									as={<Input />}
+									control={control}
+									defaultValue=""
+									name={FormData.last_review}
+								/>
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.reviews_per_month} className={classes.inputLabel}>
-							{FormData.reviews_per_month.toUpperCase()}
-						</InputLabel>
-						<Controller
-							as={<Input />}
-							control={control}
-							defaultValue=""
-							name={FormData.reviews_per_month}
-						/>
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.reviews_per_month} className={classes.inputLabel}>
+									{FormData.reviews_per_month.toUpperCase()}
+								</InputLabel>
+								<Controller
+									as={<Input />}
+									control={control}
+									defaultValue=""
+									name={FormData.reviews_per_month}
+								/>
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.calculated_host_listings_count} className={classes.inputLabel}>
-							{FormData.calculated_host_listings_count.toUpperCase()}
-						</InputLabel>
-						<Controller
-							as={<Input />}
-							control={control}
-							defaultValue=""
-							name={FormData.calculated_host_listings_count}
-						/>
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel
+									htmlFor={FormData.calculated_host_listings_count}
+									className={classes.inputLabel}>
+									{FormData.calculated_host_listings_count.toUpperCase()}
+								</InputLabel>
+								<Controller
+									as={<Input />}
+									control={control}
+									defaultValue=""
+									name={FormData.calculated_host_listings_count}
+								/>
+							</FormControl>
 
-					<FormControl required className={classes.formControl}>
-						<InputLabel htmlFor={FormData.availability_365} className={classes.inputLabel}>
-							{FormData.availability_365.toUpperCase()}
-						</InputLabel>
-						<Controller as={<Input />} control={control} defaultValue="" name={FormData.availability_365} />
-					</FormControl>
+							<FormControl required className={classes.formControl}>
+								<InputLabel htmlFor={FormData.availability_365} className={classes.inputLabel}>
+									{FormData.availability_365.toUpperCase()}
+								</InputLabel>
+								<Controller
+									as={<Input />}
+									control={control}
+									defaultValue=""
+									name={FormData.availability_365}
+								/>
+							</FormControl>
 
-					<Button type="submit" variant="contained" color="primary" className={classes.button}>
-						Submit
-					</Button>
-				</form>
+							<Button type="submit" variant="contained" color="primary" className={classes.button}>
+								Submit
+							</Button>
+						</form>
+					)}
+				</main>
 			)}
-			</main>
 		</div>
 	);
 };
@@ -323,5 +370,10 @@ const useClasses = makeStyles({
 	},
 	select: {
 		textAlign: 'left',
+	},
+	spinner: {
+		position: 'absolute',
+		left: 'calc(50vw - 20px)',
+		top: 'calc(50vh - 20px)',
 	},
 });
